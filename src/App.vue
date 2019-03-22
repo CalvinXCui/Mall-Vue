ff0d6af7841130c66daa497039d86969bedf4e4e
calvinXCui <Calvin_it@163.com>
2019-03-22 13:44:35 +0800

    第一次提交商城代码

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header/Header';
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+};
+</script>
